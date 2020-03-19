8dfd6610af1f249d7a40e464bcd3c31307b9e8c1
rtmonteiro <ryan.monteiro@aluno.ufes.br>
2020-03-19 01:10:44 -0300

    Inserted a special Footer

--- pages/index.js
+++ pages/index.js
@@ -12,35 +12,53 @@ const GlobalStyle = createGlobalStyle`
     font-family: sans-serif;
   }
   body {
-    // margin: 20px;
-    // display: flex;
-    // align-content: center;
+    /* margin: 20px; */
+    /* display: flex; */
+    /* align-content: center; */
     text-align: center;
+    /* background: black; */
   }
   h2 {
     margin-bottom: 10px;
   }
-`
+`;
 
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
-  display: flex;
+  display: grid;
   justify-content: center;
   align-items: center;
-`
+`;
 
 const CovidApp = styled.div`
-  width: 60%;
-  max-width: 700px;
-  min-width: 350px;
+  /* width: 60%; */
+  /* max-width: 700px; */
+  /* min-width: 350px; */
   background: azure;
   padding: 20px;
   border-radius: 20px;
   border: turquoise 1px solid;
-`
+  @media screen and (max-width: 748px) {
+    border-radius: 0px;
+  }
+`;
 
-export default function IndexPage () {
+const Footer = styled.div`
+  /* display: relative;
+  bottom: 10px; */
+  margin-top: 15px;
+  padding: 10px;
+  background: #99ffff;
+  border-radius: 5px;
+  font-weight: bold;
+  a {
+    /* text-decoration: inherit; */
+    color: inherit;
+  }
+`;
+
+export default function IndexPage() {
   return (
     <>
       <Head>
@@ -57,6 +75,9 @@ export default function IndexPage () {
           <Stats url="https://covid19.mathdro.id/api"></Stats>
           <br />
           <CountrySelector></CountrySelector>
+          <Footer>
+            Made with 🤍 by <a href="https://github.com/rtmonteiro">Ryan Monteiro</a>
+          </Footer>
         </CovidApp>
       </Container>
     </>
